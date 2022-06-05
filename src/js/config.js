91a518ad7bd9cc3fa6ec2a61d34a3adf324a5905
DavePolnick1 <DavePolnick1@gmail.com>
2022-06-05 01:10:14 -0500

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '90fb71db-ce89-4090-a4bd-9564ea7adc37';
+export const MODAL_CLOSE_SEC = 2.5;
